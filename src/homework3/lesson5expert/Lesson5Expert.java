7adc056317661fc8fabc8db67f7d94f9aa35d4bd
VoodoomanRUS <voodooman88@gmail.com>
2022-06-24 15:46:11 +0700

    temp commit4

--- src/homework3/lesson5expert/Lesson5Expert.java
+++ src/homework3/lesson5expert/Lesson5Expert.java
@@ -1,16 +1,18 @@
 package homework3.lesson5expert;
 
-import homework3.lesson5advanced.Lesson5Advanced;
+import lesson5.Homework3;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.SQLOutput;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -18,7 +20,10 @@ import java.util.stream.Stream;
 
 
 public class Lesson5Expert {
-    public static String regex = "pyterochka;(\\d+\\.\\d{2});(\\d+\\.\\d{2});\\d{2}/(\\d)/\\d{4}";
+    public static String dataLineRegex = "pyterochka;(\\d+\\.\\d{2});(\\d+\\.\\d{2});\\d{2}/(\\d)/\\d{4}";
+    public static String fileNameRegex = "\\p{L}+\\_(\\d{2})\\_(\\d{4}).\\p{L}+";
+    public static File[] files = new File("resource").listFiles();
+
 
     // Задача №1
     // Необходимо составить отчет о итоговой прибыли за каждый месяц по магазину pyterochka
@@ -29,22 +34,60 @@ public class Lesson5Expert {
     // и тд
     public static void zadacha1expert() throws IOException {
 
+        System.out.println(Arrays.toString(files));
+        List<String> wantedFiles = Arrays.stream(files)
+                .map(File::getName)
+                .filter(name -> name.contains("2012"))
+                .collect(Collectors.toList());
+        System.out.println(wantedFiles);
 
-        Stream<Path> resource = Files.list(Path.of("resource"));
-        ???
-        String filePath;
-        getProfit(filePath, regex);
 
+//        System.out.println(resource);
 
+//
+//        ???
+//        getFileName();
+//        getProfit(fileName, dataLine);
+//
+//
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append("Прибыль по магазину pyterochka по месяцам")
+//                .append(getDate(fileName, dataLine))
+//                .append(getProfit(fileName, dataLine))
+//                .append(", общие расходы - ")
+//                .append("\n");
+//
+//        System.out.println();
+
+    }
+
+    public static String getFileNameByMonth(Integer month) {
+            if (month < 10) {
+                String s = Homework3.class.getClassLoader().getResource("report_0" + month + "_2012.txt").toString();
+                return s;
+            } else if (month < 12) {
+
+                String s = Homework3.class.getClassLoader().getResource("report_" + month + "_2012.txt").toString();
+                return s;
+            } else {
+                System.out.println("Введите корректный номер месяца");
+                return null;
+            }
+    }
+
+    private static String getDate(String fileName, String fileNameRegex) {
+        Matcher matcher = Pattern.compile(fileNameRegex).matcher(fileName);
+        String[] dateParts = new String[2];
+        for (int i = 0; i < dateParts.length; i++) {
+            if (matcher.find()) {
+                dateParts[0] = matcher.group(1);
+                dateParts[1] = matcher.group(2);
+            }
+        }
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Прибыль по магазину pyterochka по месяцам")
-                .append(roundingUp(getProfit(filePath, regex)[1]) - roundingUp(getProfit(filePath, regex)[2]))
-                .append(", общие расходы - ")
-                .append(Lesson5Advanced.roundingUp(getProfit(filePath)[2]))
-                .append("\n");
+        stringBuilder.append(dateParts[0]).append(dateParts[1]);
 
-        System.out.println(outputString);
-
+        return stringBuilder.toString();
     }
 
     //метод округления даблов
@@ -64,12 +107,8 @@ public class Lesson5Expert {
         return values;
     }
 
-//    public static String findMonth(String group3) {
-//
-//    }
-
-    public static double getProfit(String filepath, String regex) throws IOException {
-        FileReader fileReader = new FileReader(filepath);
+    public static BigDecimal getProfit(String fileName, String regex) throws IOException {
+        FileReader fileReader = new FileReader(fileName);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
 
         double incomeSum = 0.0;
@@ -90,7 +129,8 @@ public class Lesson5Expert {
 
         double profit = incomeSum - outcomeSum;
 
-        return profit;
+
+        return roundingUp(profit);
     }
 
 
