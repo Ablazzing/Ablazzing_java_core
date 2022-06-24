30d35f9ac48534988b03a87c39a9295f545b7557
VforVeselov <nikiteveselovu@gmail.com>
2022-06-24 14:33:04 +0300

    Домашка ПРО

---
+++ src/lesson5/Zhiguli.java
@@ -0,0 +1,10 @@
+package lesson5;
+
+public class Zhiguli extends CarFactory {
+    public Zhiguli() {
+    }
+
+    public void problem() {
+        System.out.println("Жигули ломается");
+    }
+}
